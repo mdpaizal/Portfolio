3458c27eabb610b827c866e6d056791d3f382136
mdpaizal <mohamedpaizal2@gmail.com>
2025-07-14 14:44:13 +0530

    paizal

--- my-website/src/components/Hero.jsx
+++ my-website/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 // src/components/Hero.jsx
 import React from "react";
-import profilePic from "../assets/HeroImage1.jpeg"; // Add your image in public or src/assets folder
+import profilePic from "../assets/HeroImage1.jpeg"; 
+import ButtonHoverRight from "./Librarys/CV";
+// Add your image in public or src/assets folder
 
 const Hero = () => {
   return (
@@ -8,6 +10,11 @@ const Hero = () => {
       <div className="md:w-1/2 text-center md:text-left space-y-4">
         <h2 className="text-4xl font-bold">Hi, I'm<span className="text-yellow-500"> Mohamed Paizal</span></h2>
         <p className="text-lg text-gray-600">A passionate FullStack Developer creating interactive and scalable web applications.</p>
+        <a href="/MP-CV.pdf"
+        download
+        >
+          <ButtonHoverRight/>
+        </a>
       </div>
       <div className="md:w-1/2 mt-8 md:mt-0">
         <img src={profilePic} alt="Profile" className="rounded-full w-64 h-64 mx-auto shadow-lg" />
