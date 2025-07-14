2cff0bce3d8c2c64862966327dc2a931783197e3
mdpaizal <mohamedpaizal2@gmail.com>
2025-07-14 15:16:00 +0530

    Testing

--- my-website/src/components/Hero.jsx
+++ my-website/src/components/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
       <div className="md:w-1/2 text-center md:text-left space-y-4">
         <h2 className="text-4xl font-bold">Hi, I'm<span className="text-yellow-500"> Mohamed Paizal</span></h2>
         <p className="text-lg text-gray-600">A passionate FullStack Developer creating interactive and scalable web applications.</p>
-        <a href="/MP-CV.pdf"
+        {/* <a href="/MP-CV.pdf"
         download
         >
-          {/* <ButtonHoverRight/> */}
-        </a>
+          <ButtonHoverRight/>
+        </a> */}
       </div>
       <div className="md:w-1/2 mt-8 md:mt-0">
         <img src={profilePic} alt="Profile" className="rounded-full w-64 h-64 mx-auto shadow-lg" />
